d3c729effe02d0b1c0d63fbcd17932952c0467d6
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-04-30 14:08:54 -0400

    edited models and created index w/relationships

---
+++ models/BlogPost.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class BlogPost extends Model {}
+
+BlogPost.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'user',
+              key: 'id',
+            },
+          },
+          date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+          },
+        },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'blog_post',
+    }
+);
+
+module.exports = BlogPost;
