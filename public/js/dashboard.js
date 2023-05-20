4cf7412496081942647107fb7d3765f3cca55795
gabriela ortiz <gabriela_ortiz1@alumni.brown.edu>
2023-05-19 21:01:00 -0400

    delete function works

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -21,32 +21,16 @@ const newFormHandler = async (event) => {
   }
 };
 
-const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
-
-    const response = await fetch(`/api/blogpost/${id}`, {
-      method: 'DELETE',
-    });
-
-    if (response.ok) {
-      document.location.replace('/');
-    } else {
-      alert('Failed to delete project');
-    }
-  }
-};
-
 const deleteBlogPost = async (blogPostId) => {
   try {
-    const response = await fetch(`/api/blogposts/${blogPostId}`, {
+    const response = await fetch(`/api/blogpost/${blogPostId}`, {
       method: 'DELETE',
     });
     
     if (response.ok) {
       // Delete was successful, perform any necessary UI updates or redirects
       console.log('Blog post deleted successfully');
-      // For example, you can reload the page to reflect the updated list of blog posts
+      //reload the page to reflect the updated list of blog posts
       window.location.reload();
     } else {
       // Delete request failed
@@ -63,6 +47,6 @@ document
   .querySelector('.new-blogpost-form')
   .addEventListener('submit', newFormHandler);
 
-document
-  .querySelector('.blogpost-list')
-  .addEventListener('click', delButtonHandler);
+// document
+//   .querySelector('.blogpost-list')
+//   .addEventListener('click', delButtonHandler);
